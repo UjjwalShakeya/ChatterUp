a22cfc72206719057d5244b176145373d16f1a91
UjjwalShakeya <ujjwalshakeya1@gmail.com>
2026-04-12 20:01:57 +0530

    feat(rightsidebar): add user details section and media gallery with improved layout

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -92,7 +92,9 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
         <div ref={scrollEnd}></div>
       </div>
 
+        {/* --------------------Bottom Area---------- */}
       <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
+
         <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
             <input type="text" placeholder='Send a message' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
             <input type="file" id='image' accept='image/png, image/jpeg' hidden/>
@@ -100,6 +102,7 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
               <img src={assets.gallery_icon} alt="" className='w-6 mr-2 cursor-pointer'/>
             </label>
         </div>
+        
           <img src={assets.send_button} alt="" className='w-7 cursor-pointer' />
       </div>
 
